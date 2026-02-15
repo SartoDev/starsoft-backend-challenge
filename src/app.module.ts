78a255036719bab8053bf57a6c6cfcad47eba015
SartoDev <leonardosarto1@gmail.com>
2026-02-15 19:59:03 -0300

    Add Throttler, config and jwt modules to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { ConfigModule } from "@nestjs/config";
+import { ThrottlerModule } from "@nestjs/throttler";
+import { JwtModule } from "@nestjs/jwt";
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60,
+          limit: 50,
+        },
+      ],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: "1h",
+      },
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
